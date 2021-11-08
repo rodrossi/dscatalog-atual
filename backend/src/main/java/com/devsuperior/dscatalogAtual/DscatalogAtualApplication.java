1a3a7e476246f5545aeb5c81224e2e0ad0688159
Rodrigo <rodrigoormonde@gmail.com>
2021-11-08 18:27:21 -0300

    Backend prroject created

---
+++ backend/src/main/java/com/devsuperior/dscatalogAtual/DscatalogAtualApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalogAtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogAtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogAtualApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscatalogAtual/DscatalogAtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalogAtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogAtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
